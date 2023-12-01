47cd27173287fed8d87d941388b546bec69162cc
Kumbulya <ttrudnoe@mail.ru>
2023-12-02 00:15:34 +0700

    Появился каталог, пользователь теперь может добавлять товары с фотографиями, в профиле так же появилась возможность поставить и сменить фотографию

---
+++ app/web/handlers_main.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type User struct {
+	Id       int
+	Login    string
+	Password string
+	Page     string
+	Avatar   bool
+}
+
+type Product struct {
+	Id           int
+	Product_name string
+	Category     string
+	Seller       string
+	Seller_name  string
+	Description  string
+}
+
+func home(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+
+	rows, err := db.Query("SELECT katalog.id, katalog.product_name, katalog.category, katalog.seller, katalog.description, users.login FROM katalog INNER JOIN users ON katalog.seller = users.page")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+	var products []Product
+	for rows.Next() {
+		var p Product
+		if err := rows.Scan(&p.Id, &p.Product_name, &p.Category, &p.Seller, &p.Description, &p.Seller_name); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		products = append(products, p)
+	}
+
+	files := []string{
+		"html/tmpl/home.page.html",
+		"html/tmpl/base.layout.html",
+		"html/tmpl/footer.partial.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	err = ts.Execute(w, products)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func account(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	id := r.URL.Query().Get("id")
+
+	query := fmt.Sprintf("SELECT COUNT(*) FROM `users` WHERE `page` = '%s'", id)
+
+	res, err := db.Query(query)
+	if err != nil {
+		http.Error(w, "Error executing query", http.StatusInternalServerError)
+		return
+	}
+	defer res.Close()
+
+	var count int
+	for res.Next() {
+		err := res.Scan(&count)
+		if err != nil {
+			http.Error(w, "Error scanning result", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if count != 1 {
+		w.Write([]byte("Такого аккаунта нет"))
+		return
+	}
+
+	query = fmt.Sprintf("SELECT * FROM `users` WHERE `page` = '%s'", id)
+
+	res, err = db.Query(query)
+	if err != nil {
+		http.Redirect(w, r, "/sign_in", http.StatusMovedPermanently)
+	}
+
+	defer res.Close()
+
+	user := User{}
+	for res.Next() {
+		err := res.Scan(&user.Id, &user.Login, &user.Password, &user.Page)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+	}
+
+	user.Avatar = true
+	if _, err := os.Stat("html/static/img/account_" + user.Page + ".jpg"); os.IsNotExist(err) {
+		user.Avatar = false
+	}
+
+	files := []string{
+		"html/tmpl/account.page.html",
+		"html/tmpl/base.layout.html",
+		"html/tmpl/footer.partial.html",
+	}
+
+	templ, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = templ.Execute(w, user)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	page := r.FormValue("page")
+	file, _, err := r.FormFile("account_avatar")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	dst, err := os.Create("html/static/img/account_" + page + ".jpg")
+
+	if err != nil {
+		log.Println("Pizda rulu")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/account?id="+page, http.StatusMovedPermanently)
+
+}
+
+func upload_product(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	new_product := Product{}
+
+	user := r.FormValue("user")
+	product_name := r.FormValue("product_name")
+	product_category := r.FormValue("product_category")
+	product_description := r.FormValue("product_description")
+
+	query := fmt.Sprintf("INSERT INTO `katalog` (`product_name`, `category`, `seller`, `description`) VALUES ('%s','%s','%s','%s')",
+		product_name, product_category, user, product_description)
+	insert, err := db.Query(query)
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer insert.Close()
+
+	query = fmt.Sprintf("SELECT `id` FROM `katalog` WHERE `product_name` = '%s'", product_name)
+	res, err := db.Query(query)
+	if err != nil {
+		http.Redirect(w, r, "/sign_in", http.StatusMovedPermanently)
+	}
+
+	for res.Next() {
+		err := res.Scan(&new_product.Id)
+		if err != nil {
+			http.Error(w, "Error scanning result", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	file, _, err := r.FormFile("product_avatar")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	dst, err := os.Create("html/static/img/product_" + strconv.Itoa(new_product.Id) + ".jpg")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/account?id="+user, http.StatusMovedPermanently)
+}
--- app/web/main.go
+++ app/web/main.go
@@ -19,7 +19,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		home(w, r)
+		home(w, r, db)
 	})
 	mux.HandleFunc("/sign_up", func(w http.ResponseWriter, r *http.Request) {
 		sign_up(w, r, db)
@@ -31,8 +31,16 @@ func main() {
 		account(w, r, db)
 	})
 
-	fileServer := http.FileServer(http.Dir("html/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		upload(w, r)
+	})
+
+	mux.HandleFunc("/upload_product", func(w http.ResponseWriter, r *http.Request) {
+		upload_product(w, r, db)
+	})
+
+	fileServer := http.FileServer(http.Dir("html/static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
 	http.ListenAndServe(":1337", mux)
 
