c70ef0312dfb6dc9c7b7f66f7d3558ef4c728e74
Kumbulya <ttrudnoe@mail.ru>
2023-12-02 04:43:02 +0700

    Поменял местами sign in и sign up

--- app/web/handlers_sign.go
+++ app/web/handlers_sign.go
@@ -10,6 +10,52 @@ import (
 
 func sign_up(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if r.Method == http.MethodPost {
+		login_in := r.FormValue("login_si")
+		pass_in := r.FormValue("pass_si")
+		query := fmt.Sprintf("SELECT COUNT(*) FROM `users` WHERE `login` = '%s'", login_in)
+		res, err := db.Query(query)
+		if err != nil {
+			http.Error(w, "Error executing query", http.StatusInternalServerError)
+			return
+		}
+		defer res.Close()
+
+		var count int
+		for res.Next() {
+			err := res.Scan(&count)
+			if err != nil {
+				http.Error(w, "Error scanning result", http.StatusInternalServerError)
+				return
+			}
+		}
+
+		if count > 0 {
+
+			return
+		}
+
+		page := rand.Intn(100000)
+		query = fmt.Sprintf("INSERT INTO `users` (`login`, `password`, `page`) VALUES ('%s','%s','%d')", login_in, pass_in, page)
+		insert, err := db.Query(query)
+		if err != nil {
+			log.Println(err)
+		}
+
+		defer insert.Close()
+		http.Redirect(w, r, "/sign_up", http.StatusMovedPermanently)
+
+	} else if r.Method == http.MethodGet {
+		http.ServeFile(w, r, "html/tmpl/sign_in.html")
+	} else {
+
+		http.Error(w, "Метод запрещен!", http.StatusMethodNotAllowed)
+		return
+	}
+
+}
+
+func sign_in(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	if r.Method == http.MethodPost {
 		login_up := r.FormValue("login")
 		pass_up := r.FormValue("pass")
 		query := fmt.Sprintf("SELECT COUNT(*) FROM `users` WHERE `login` = '%s' ", login_up)
@@ -59,49 +105,4 @@ func sign_up(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		return
 	}
 
-}
-
-func sign_in(w http.ResponseWriter, r *http.Request, db *sql.DB) {
-	if r.Method == http.MethodPost {
-		login_in := r.FormValue("login_si")
-		pass_in := r.FormValue("pass_si")
-		query := fmt.Sprintf("SELECT COUNT(*) FROM `users` WHERE `login` = '%s'", login_in)
-		res, err := db.Query(query)
-		if err != nil {
-			http.Error(w, "Error executing query", http.StatusInternalServerError)
-			return
-		}
-		defer res.Close()
-
-		var count int
-		for res.Next() {
-			err := res.Scan(&count)
-			if err != nil {
-				http.Error(w, "Error scanning result", http.StatusInternalServerError)
-				return
-			}
-		}
-
-		if count > 0 {
-
-			return
-		}
-
-		page := rand.Intn(100000)
-		query = fmt.Sprintf("INSERT INTO `users` (`login`, `password`, `page`) VALUES ('%s','%s','%d')", login_in, pass_in, page)
-		insert, err := db.Query(query)
-		if err != nil {
-			log.Println(err)
-		}
-
-		defer insert.Close()
-		http.Redirect(w, r, "/sign_up", http.StatusMovedPermanently)
-
-	} else if r.Method == http.MethodGet {
-		http.ServeFile(w, r, "html/tmpl/sign_in.html")
-	} else {
-
-		http.Error(w, "Метод запрещен!", http.StatusMethodNotAllowed)
-		return
-	}
 }
